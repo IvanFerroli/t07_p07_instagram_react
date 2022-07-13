52e4fd9a2b6b80a00ce32e7f43df9666049dee5b
ivan <ivanilson.ferreira.mec@gmail.com>
2022-07-13 15:21:15 -0300

    feat: Stories component errors fixed

--- components/Posts.js
+++ components/Posts.js
@@ -5,7 +5,7 @@ export default function Posts() {
                 <div class="post-top">
                     <a href="#">
                         <div class="user-info">
-                            <img src="../assets/img/hoonigan.jpg" alt="Hoonigan">
+                            <img src="../assets/img/hoonigan.jpg" alt="Hoonigan"></img>
                             <p><strong>hoonigan</strong></p>
                         </div>
                     </a>
@@ -15,7 +15,7 @@ export default function Posts() {
                 </div>
                 <div class="post-middle">
                     <a href="#">
-                        <img src="../assets/img/hoonicorn_v2.webp" alt="">
+                        <img src="../assets/img/hoonicorn_v2.webp" alt=""></img>
                     </a>
                 </div>
                 <div class="post-bottom">
@@ -36,17 +36,18 @@ export default function Posts() {
                         </a>
                     </div>
                     <div class="likes">
-                        <a href="#"><img src="../assets/img/respondeai2.svg" alt="RespondeAi"></a>
+                        <a href="#"><img src="../assets/img/respondeai2.svg" alt="RespondeAi"></img></a>
                         <p>Curtido por <a href="#"><strong>respondeai</strong></a> e <a href="#"><strong>outras 101.523
                                     pessoas</strong></a></p>
                     </div>
                 </div>
             </div>
+
             <div class="post">
                 <div class="post-top">
                     <a href="#">
                         <div class="user-info">
-                            <img src="../assets/img/biela_turbo.jpg" alt="biela turbo">
+                            <img src="../assets/img/biela_turbo.jpg" alt="biela turbo"></img>
                             <p><strong>bielaturbo</strong></p>
                         </div>
                     </a>
@@ -55,7 +56,7 @@ export default function Posts() {
                     </a>
                 </div>
                 <div class="post-middle">
-                    <img src="../assets/img/widebody_chevette.jpg" alt="media">
+                    <img src="../assets/img/widebody_chevette.jpg" alt="media"></img>
                 </div>
                 <div class="post-bottom">
                     <div class="icons">
@@ -75,7 +76,7 @@ export default function Posts() {
                         </a>
                     </div>
                     <div class="likes">
-                        <a href="#"><img src="../assets/img/driven-logo.jpeg" alt="driven"></a>
+                        <a href="#"><img src="../assets/img/driven-logo.jpeg" alt="driven"></img></a>
                         <p>Curtido por <a href="#"><strong>driven</strong></a> e <a href="#"><strong>outras 99.153
                                     pessoas</strong></a></p>
                     </div>
--- components/Stories.js
+++ components/Stories.js
@@ -1,33 +1,67 @@
-export default function Header() {
+export default function Stories() {
     return(
        <>
-        <header>
-            <div class="top">
-                <div class="logo">
-                    <a href="#">
-                        <ion-icon name="logo-instagram"></ion-icon>
-                    </a>
-                    <div class="separador not-mobile"></div>
-                    <a href="#"><img class="not-mobile" src="../src/assets/img/instagram-logo.svg" alt="Instagram Logo"/></a>
-                </div>
-                <div class="search-bar not-mobile">Pesquisar</div>
-                <img class="mobile" src="../src/assets/img/instagram-logo.svg" alt="Instagram Logo"></img>
-                <div class="menu">
-                    <a href="#">
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </a>
-                    <a href="#">
-                        <ion-icon name="compass-outline" class="not-mobile"></ion-icon>
-                    </a>
-                    <a href="#">
-                        <ion-icon name="heart-outline" class="not-mobile"></ion-icon>
-                    </a>
-                    <a href="#">
-                        <ion-icon name="person-outline" class="not-mobile"></ion-icon>
-                    </a>
-                </div>
+            <div class="stories">
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/biela_turbo.jpg"/>
+                        <p>bielaturbo</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/respondeai2.svg"/>
+                        <p>respondeai</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/midnight_rbz.jpg"/>
+                        <p>midnight_rbz</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/donut_media.jpeg"/>
+                        <p>donut_media</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/driven-logo.jpeg"/>
+                        <p>driven</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/hoonigan.jpg"/>
+                        <p>hoonigan</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/topgear.jpeg"/>
+                        <p>topgear</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="single-story">
+                        <img class="story-border" src="../src/assets/img/stories_background.svg"/>
+                        <img class="profile-pic" src="../src/assets/img/cartrottle.jpg"/>
+                        <p>cartrottle</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="seta not-mobile"><img src="../src/assets/img/arrow.svg" alt="Scroll"/></div>
+                </a>
             </div>
-        </header>   
         </>
     )
 }
